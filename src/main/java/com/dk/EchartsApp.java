0c089995dcc0d6bdfba22e7852d1011adf6486d8
孙宇宇 <sun819569779@163.com>
2021-02-19 20:29:52 +0800

    测试

---
+++ src/main/java/com/dk/EchartsApp.java
@@ -0,0 +1,16 @@
+package com.dk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: 你哥
+ * @Date: 2021/1/7 09:50
+ * @Description:
+ */
+@SpringBootApplication
+public class EchartsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EchartsApp.class,args);
+    }
+}
